d5d5fd8683417a09b3cf548f2480649743919298
Gasper Zejn <zejn@kiberpipa.org>
2011-11-29 08:46:18 +0100

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,11 @@
+
+pypdf2xml
+=========
+
+This project started as an alternative to poppler's pdftoxml, which didn't
+properly decode CID Type2 fonts in PDFs. This script requires pdfminer.
+
+License
+=======
+
+Public domain.
---
+++ setup.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+from distutils.core import setup
+
+setup(name='pypdf2xml',
+      version='0.1',
+      description='A reimplementation of pdftoxml in Python, using pdfMiner. Handles unicode characters better.',
+      author=u'Gašper Žejn'.encode('utf-8'),
+      author_email='zejn@owca.info',
+      url='http://zejn.net/labs/',
+      scripts=['pdf2xml', 'pdfxml2csv'],
+      install_requires=['pdfminer>=20110515', 'lxml'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          ]
+     )
+
+
